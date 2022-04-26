d866cca7e8fa7ca6c0acde92862b29bd3c82a3e9
BanulaMahen <banulakumarage@gmail.com>
2022-04-26 23:38:22 +0530

    folder structure

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+define('DB_NAME','homepharmasist');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('HOST','127.0.0.1');
+
+define('DEFAULT_CONTROLLER','Home');// default controller
+define('SROOT','/home_pharmasist/');
+
+define('DEFAULT_LAYOUT','default');
+define('SITE_TITLE','Home Pharmasist');
+
+define('CURRENT_USER_SESSION_NAME','home_pharmasist_current_user'); // session name for logged in user
