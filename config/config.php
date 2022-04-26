1e0d2bf2d78b3e15159f971b5ede7e0019f2deca
BanulaMahen <banulakumarage@gmail.com>
2022-04-26 23:46:45 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,14 +1,14 @@
 <?php 
 
-define('DB_NAME','homepharmasist');
+define('DB_NAME','ehotelier');
 define('DB_USER','root');
 define('DB_PASSWORD','');
 define('HOST','127.0.0.1');
 
 define('DEFAULT_CONTROLLER','Home');// default controller
-define('SROOT','/home_pharmasist/');
+define('SROOT','/ehotelier');
 
 define('DEFAULT_LAYOUT','default');
-define('SITE_TITLE','Home Pharmasist');
+define('SITE_TITLE','eHotelier');
 
-define('CURRENT_USER_SESSION_NAME','home_pharmasist_current_user'); // session name for logged in user
+define('CURRENT_USER_SESSION_NAME','ehotelier_current_user'); // session name for logged in user
