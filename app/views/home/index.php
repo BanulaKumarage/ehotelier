d5a668e4bfb8df6d18f42f45414aa9d4c9a7e76f
Sathsarani Kapukotuwa <sathsaranikapukotuwa@gmail.com>
2022-05-27 20:01:18 +0530

    css for home page added

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,15 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title> Home </title>
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap">
 </head>
+
 <body>
-    <a href="<?=SROOT?>/CustomerRegister/login">Customer Login</a><br>
-    <a href="<?=SROOT?>/CustomerRegister/signup">Join now</a>
-
-<!--    ====================================-->
-    <br>
-    <a href="<?=SROOT?>/EmployeeRegister/login">Employee Login</a>
-
+    <div class="banner">
+        <div class="navbar">
+            <img src="images/logo-1.png" class="logo">
+        </div>
+        <div class="content">
+            <h1> Welcome to eHotelier </h1> <br> <br>
+            <div>
+                <ul>
+                <li><a href="<?=SROOT?>/CustomerRegister/login">Customer Login</a></li>
+                    <li><a href="<?=SROOT?>/CustomerRegister/signup">Join now</a></li>
+                    <li><a href="<?=SROOT?>/EmployeeRegister/login">Employee Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
 </body>
 </html>
