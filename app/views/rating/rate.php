f086cf9f7ed732c308b76c6f29aff7c5a6be8896
BanulaMahen <banulakumarage@gmail.com>
2022-05-28 22:58:03 +0530

    Update rate.php

--- app/views/rating/rate.php
+++ app/views/rating/rate.php
@@ -36,7 +36,8 @@ $customer = Customer::currentLoggedInCustomer();
 
     </div>
 
-    <?php
+    <?php if ($this->ratings && count($this->ratings)){ ?>
+        <?php
 
         $count = array(1=>0,2=>0,3=>0,4=>0,5=>0);
  
@@ -46,49 +47,52 @@ $customer = Customer::currentLoggedInCustomer();
             @$count[$r['value']]++;
         }
 
-        $overall = (1 * $count[1] + 2 * $count[2] + 3 * $count[3] + 4 * $count[4] + 5 * $count[5])/count($this->ratings);
-    ?>
+            $overall = (1 * $count[1] + 2 * $count[2] + 3 * $count[3] + 4 * $count[4] + 5 * $count[5])/count($this->ratings);
+        ?>
 
-    <p><?php echo (float) number_format($overall,2)?> out of <?php echo count($this->ratings)?> customer ratings </p>
+        <p><?php echo (float) number_format($overall,2)?> out of <?php echo count($this->ratings)?> customer ratings </p>
 
-    <table>
-        <tr>
-            <td>1 star: </td><td><?php echo $count[1]?></td>
-        </tr>
-        <tr>
-            <td>2 star: </td><td><?php echo $count[2]?></td>
-        </tr>
-        <tr>
-            <td>3 star:</td><td><?php echo $count[3]?></td>
-        </tr>
-        <tr>
-            <td>4 star: </td><td><?php echo $count[4]?></td>
-        </tr>
-        <tr>
-            <td>5 star: </td><td><?php echo $count[5]?></td>
-        </tr>
-    </table>
-
-    <br>
-
-    <div>
         <table>
-            <?php
-                foreach ($this->ratings as $rating) {
-                    $rating = (array) $rating;
-                    ?>
-                    <tr>
-                    <td>
-                        <?php echo $rating['value'];?>
-                    </td>
-                    <td>
-                        <?php echo $rating['description'];?>
-                    </td>
-                    </tr>
-                <?php }
-            ?>
+            <tr>
+                <td>1 star: </td><td><?php echo $count[1]?></td>
+            </tr>
+            <tr>
+                <td>2 star: </td><td><?php echo $count[2]?></td>
+            </tr>
+            <tr>
+                <td>3 star:</td><td><?php echo $count[3]?></td>
+            </tr>
+            <tr>
+                <td>4 star: </td><td><?php echo $count[4]?></td>
+            </tr>
+            <tr>
+                <td>5 star: </td><td><?php echo $count[5]?></td>
+            </tr>
         </table>
-    </div>
+
+        <br>
+
+        <div>
+            <table>
+                <?php
+                    foreach ($this->ratings as $rating) {
+                        $rating = (array) $rating;
+                        ?>
+                        <tr>
+                        <td>
+                            <?php echo $rating['value'];?>
+                        </td>
+                        <td>
+                            <?php echo $rating['description'];?>
+                        </td>
+                        </tr>
+                    <?php }
+                ?>
+            </table>
+        </div>
+
+
+    <?php } ?>
 
 </body>
 
