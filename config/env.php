f5aeea13401b3900834b0835084c4b294ff6e357
BanulaMahen <banulakumarage@gmail.com>
2022-05-30 19:32:47 +0530

    v2

--- config/env.php
+++ config/env.php
@@ -1,11 +1,11 @@
 <?php 
 
-//define('DB_NAME','sql6495984');
-//define('DB_USER','sql6495984');
-//define('DB_PASSWORD','PTg7NcSgI5');
-//define('HOST','sql6.freesqldatabase.com');
+define('DB_NAME','sql6495984');
+define('DB_USER','sql6495984');
+define('DB_PASSWORD','PTg7NcSgI5');
+define('HOST','sql6.freesqldatabase.com');
 
-define('DB_NAME','ehotelier');
-define('DB_USER','root');
-define('DB_PASSWORD','');
-define('HOST','127.0.0.1');
+// define('DB_NAME','ehotelier');
+// define('DB_USER','root');
+// define('DB_PASSWORD','');
+// define('HOST','127.0.0.1');
