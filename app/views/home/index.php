59575482073fb97736428d5b565bfb55ff1670c6
BanulaMahen <banulakumarage@gmail.com>
2022-06-06 13:25:33 +0530

    Update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Home </title>
+    <title> eHotelier </title>
     <link rel="icon" href="<?=SROOT?>images/favicon2.jpg" type="image/jpg">
     <link rel="stylesheet" href="<?=SROOT?>css/home.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
