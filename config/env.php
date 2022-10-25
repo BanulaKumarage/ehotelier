ce9e528ea622465d9b142d40fc742fc867d6dc0e
BanulaMahen <banulakumarage@gmail.com>
2022-10-25 14:50:51 +0530

    db updated

--- config/env.php
+++ config/env.php
@@ -1,14 +1,14 @@
 <?php 
 
 // cloud-based database
-// define('DB_NAME','sql6495984');
-// define('DB_USER','sql6495984');
-// define('DB_PASSWORD','PTg7NcSgI5');
-// define('HOST','sql6.freesqldatabase.com');
+define('DB_NAME','sql6529088');
+define('DB_USER','sql6529088');
+define('DB_PASSWORD','AF23T39zhe');
+define('HOST','sql6.freesqldatabase.com');
 
 
 //local database
-define('DB_NAME','ehotelier');
-define('DB_USER','root');
-define('DB_PASSWORD','');
-define('HOST','127.0.0.1');
+// define('DB_NAME','ehotelier');
+// define('DB_USER','root');
+// define('DB_PASSWORD','');
+// define('HOST','127.0.0.1');
